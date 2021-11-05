da4993ec3b08283e1bc1675bd6a232f02421c658
Beatriz Rangel <raangelbeatriz@gmail.com>
2021-11-04 22:47:15 -0300

    add format date on adapter

--- app/src/main/java/com/rangelbeatriz/fitnesstracker/ListAdapter.java
+++ app/src/main/java/com/rangelbeatriz/fitnesstracker/ListAdapter.java
@@ -1,5 +1,6 @@
 package com.rangelbeatriz.fitnesstracker;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
     List<Register> register;
@@ -42,14 +46,29 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder
         }
 
         public void bind(Register item){
+
             TextView imc_response = itemView.findViewById(R.id.item_imc_response);
             TextView imc_date = itemView.findViewById(R.id.item_imc_date);
             TextView imc_value = itemView.findViewById(R.id.item_imc_value);
 
-            imc_response.setText(Double.toString(item.response));
-            imc_date.setText(item.createdDate);
+            imc_response.setText(String.valueOf(item.response));
+            imc_date.setText(formatDate(item.createdDate));
             imc_value.setText(item.type);
 
+        }
+
+        String formatDate(String createdDate){
+            String formatted = "";
+            try{
+                SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", new Locale("pt", "BR"));
+                SimpleDateFormat dateBR = new SimpleDateFormat("dd-MM-yyyy HH:mm", new Locale("pt", "br"));
+                Date simpleDate = date.parse(createdDate);
+                formatted = dateBR.format(simpleDate);
+            }
+            catch (Exception e){
+                Log.d("Date", e.getMessage(), e);
+            }
+            return formatted;
         }
     }
 }
